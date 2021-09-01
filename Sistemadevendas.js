3f77c58110205f3c68b6963d56e77ebcf8eb435d
Gabriel FS <gabrielnup@outlook.com>
2021-08-31 22:03:54 -0300

    31-08

--- Sistemadevendas.js
+++ Sistemadevendas.js
@@ -3,7 +3,7 @@ let Sistemadevendas = () => {
     let vendedores = []
     let vendas = []
     do {
-        opcao = Number(prompt(`Informe: \n1. Cadastrar Vendedor \n2. Cadastrar venda \n3. Procurar venda \n. Sair`))
+        opcao = Number(prompt(`Informe: \n1. Cadastrar Vendedor \n2. Cadastrar venda \n3. Procurar venda por mês e vendedor \n4. Soma venda por vendedor \n5.`))
         switch (opcao) {
             case 1: let objeto = {
                 codigo: Number(prompt(`Informe o código`)),
@@ -50,17 +50,34 @@ let Sistemadevendas = () => {
             case 3: let codigo = Number(prompt(`Informe o codigo do vendedor`));
                     let mes = Number(prompt(`Informe o mês da venda`))
                     let achoupv = false
-                    for (let i = 0;  < vendas.length; i++) {
+                    for (let i = 0; i < vendas.length; i++) {
                         if ((vendas[i].codigo == codigo) && (vendas[i].mes == mes)) {
                            console.log(`O valor da venda do funcionário ${codigo} no mês ${mes} foi ${vendas[i].Valor}`)
                            achoupv = true 
                         }
                     }
-                    if (!achou) {
+                    if (!achoupv) {
                         console.log(`Venda não encontrada para este funcionário este mês.`)
                     }
                 break;
-            case 4: alert(`O Programa será finalizado`)
+            case 4: let codigo = Number(prompt(`Informe o código do vendedor`));
+                    let achousoma = false
+                    soma = 0
+                    // Percorrer o vetor de vendas
+                    for (let i = 0; i < vendas.length; i++){
+                        if (vendas[i].codigo == codigo) {
+                            soma = soma + vendas[i].Valor
+                            achousoma = true
+                        }
+                    }
+                    if (!achousoma) {
+                        console.log(`Venda não encontrada para este funcionário`)
+                    }
+                    else{
+                        console.log(`O Total de vendas do vendedor ${codigo} foi ${soma}`)
+                    }
+                break;
+            case 5: alert(`O Programa será finalizado`)
                 break;
             default: alert(`Opção invalida`)
         }
